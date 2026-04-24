2eb7734a102fcf80a9bdd1513df3c741a96e3e57
Jetross <jetrossneri07@gmail.com>
2026-04-24 09:36:46 +0800

    feat: add logo

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -1,10 +1,15 @@
 import { ModeToggle } from "./mode-toggle";
+import Image from "next/image";
+import logo from "@/app/icon.png";
 
 export function Navbar() {
   return (
     <nav className="w-full border-b">
       <div className="container mx-auto flex h-16 items-center justify-between">
-        <div className="text-lg font-bold">Aura</div>
+        <div className="flex flex-row text-lg font-bold gap-2">
+          <Image src={logo.src} alt="Logo" height={28} width={28} />
+          Aura
+        </div>
         <div>
           <ModeToggle />
         </div>
